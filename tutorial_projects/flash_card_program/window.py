a2b73ffb9b23b10cbb20221a925c5d7235b2b7a1
Eius <34424590+Eius@users.noreply.github.com>
2023-01-30 17:40:35 +0100

    Updated flash_card_program
    Added sound effects

--- tutorial_projects/flash_card_program/window.py
+++ tutorial_projects/flash_card_program/window.py
@@ -1,8 +1,14 @@
 import tkinter as tk
 from data_manager import DataManager
+from winsound import PlaySound, SND_ASYNC, SND_FILENAME
 
 BG_COLOR = "#B1DDC6"
 CARD_FLASH_INTERVAL = 2500
+FRONT_CARD_IMAGE = "images/card_front.png"
+BACK_CARD_IMAGE = "images/card_back.png"
+WRONG_BUTTON_IMAGE = "images/wrong.png"
+RIGHT_BUTTON_IMAGE = "images/right.png"
+NEXT_CARD_SOUND = "sounds/next_card.wav"
 
 
 class Window(tk.Tk):
@@ -24,16 +30,16 @@ class Window(tk.Tk):
         self.minsize(width=900, height=576)
 
         # Front Canvas
-        self.front_image = tk.PhotoImage(file="images/card_front.png")
-        self.back_image = tk.PhotoImage(file="images/card_back.png")
+        self.front_image = tk.PhotoImage(file=FRONT_CARD_IMAGE)
+        self.back_image = tk.PhotoImage(file=BACK_CARD_IMAGE)
         self.canvas = tk.Canvas(self, height=526, width=800, background=BG_COLOR, highlightthickness=0)
 
         # Wrong button
-        self.wrong_image = tk.PhotoImage(file="images/wrong.png")
+        self.wrong_image = tk.PhotoImage(file=WRONG_BUTTON_IMAGE)
         self.wrong_button = tk.Button(self, image=self.wrong_image, highlightthickness=0, relief="flat",
                                       border=0, command=self.on_wrong_click)
         # Right button
-        self.right_image = tk.PhotoImage(file="images/right.png")
+        self.right_image = tk.PhotoImage(file=RIGHT_BUTTON_IMAGE)
         self.right_button = tk.Button(self, image=self.right_image, highlightthickness=0, relief="flat",
                                       border=0, command=self.on_right_click)
 
@@ -68,15 +74,17 @@ class Window(tk.Tk):
         self.current_card = "back"
 
     def on_wrong_click(self):
-        self.data_manager.save_unknown_words()
-        self.current_data = self.data_manager.pick_random_word()
-        self.show_front_card(self.current_data["Dutch"])
-        self.start_timer()
+        self.next_card()
 
     def on_right_click(self):
         self.data_manager.remove_known_word(self.current_data)
+        self.next_card()
+
+    def next_card(self):
+        self.data_manager.save_unknown_words()
+        PlaySound(NEXT_CARD_SOUND, SND_FILENAME | SND_ASYNC)
         self.current_data = self.data_manager.pick_random_word()
-        self.show_front_card(self.current_data["Dutch"])
+        self.show_front_card(self.current_data[self.column_1_header])
         self.start_timer()
 
     def start_timer(self):
@@ -88,9 +96,7 @@ class Window(tk.Tk):
 
     def flip_card(self):
         if self.current_card == "front":
-            self.show_back_card(self.current_data["English"])
+            self.show_back_card(self.current_data[self.column_2_header])
         else:
-            self.show_front_card(self.current_data["Dutch"])
+            self.show_front_card(self.current_data[self.column_1_header])
         self.timer_handle = self.after(CARD_FLASH_INTERVAL, self.flip_card)
-
-
